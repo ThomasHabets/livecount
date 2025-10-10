f31fcd019f095f8ac8c5cf112477970564b5bbc2
Thomas Habets <thomas@habets.se>
2025-10-10 11:47:24 +0100

    Switch from potentially deadlocking select to safer try_join

--- src/filters.rs
+++ src/filters.rs
@@ -11,7 +11,7 @@ use warp::Filter;
 use warp::Reply;
 
 use crate::registry::Registry;
-use crate::registry::{UPDATES_SENT, WS_RX_TYPE};
+use crate::registry::{TIMEOUTS, UPDATES_SENT, WS_RX_TYPE};
 
 mod static_assert {
     /// This asserts that ping life is at least 30 seconds before end of
@@ -73,102 +73,173 @@ async fn livecount_ws_map_upgrade(
 ) {
     debug!("livecount_ws_map_upgrade()");
     debug!("WS upgrade on {url} by {remote}");
+
+    // Split the websocket so that we can give it to separate futures.
     let (mut tx, mut rx) = websocket.split();
 
+    // See https://biriukov.dev/docs/async-rust-tokio-io/3-tokio-io-patterns/ pattern.
     let mut handle = reg.register(url.as_str()).await.unwrap();
-    {
-        let deadline = tokio::time::Instant::now() + MAX_WS_LIFE;
-        let timeout = tokio::time::sleep_until(deadline);
-        let deadline = tokio::time::Instant::now() + MAX_WS_LIFE_PING;
-        let timeout_ping = tokio::time::sleep_until(deadline);
-        tokio::pin!(timeout);
-        tokio::pin!(timeout_ping);
-        loop {
-            let mut new_sleep = None;
-            let mut new_sleep_ping = None;
-            tokio::select! {
-                msg = handle.next() => {
-                    // TODO: batch updates.
-                    if let Some(msg) = msg {
-                        match websocket_send(&mut tx, Message::text(format!("{msg}"))).await {
-                            Err(e) => {
-                                warn!("Error sending on websocket: {e}");
-                                UPDATES_SENT.with_label_values(&["data", &e]).inc();
-                                break;
-                            },
-                            Ok(_) => {
-                                UPDATES_SENT.with_label_values(&["data", "ok"]).inc();
-                            },
-                        }
-                    }
-                },
-                wsmsg = rx.next() => {
-                    match wsmsg {
-                        None => {
-                            debug!("Got None message, disconnecting");
-                            WS_RX_TYPE.with_label_values(&["none"]).inc();
-                            break;
-                        },
-                        Some(Ok(ref m)) => {
-                            debug!("Got a message: {m:?}");
-                            new_sleep_ping = Some(MAX_WS_LIFE_PING);
-                            if m.is_close(){
-                                debug!("WS Disconnection: {:?}", wsmsg);
-                                WS_RX_TYPE.with_label_values(&["away"]).inc();
-                                break;
-                            } else if m.is_ping() {
-                                WS_RX_TYPE.with_label_values(&["ping"]).inc();
-                                new_sleep = Some(MAX_WS_LIFE);
-                            } else if m.is_text() {
-                                WS_RX_TYPE.with_label_values(&["text"]).inc();
-                                new_sleep = Some(MAX_WS_LIFE);
-                            } else if m.is_binary() {
-                                WS_RX_TYPE.with_label_values(&["binary"]).inc();
-                                new_sleep = Some(MAX_WS_LIFE);
-                            } else if m.is_pong() {
-                                WS_RX_TYPE.with_label_values(&["pong"]).inc();
-                                new_sleep = Some(MAX_WS_LIFE);
-                            } else {
-                                WS_RX_TYPE.with_label_values(&["unknown"]).inc();
-                                error!("Unknown message type: {wsmsg:?}");
-                            }
-                        },
-                        Some(Err(e)) => {
-                            WS_RX_TYPE.with_label_values(&[format!("{e}")]).inc();
-                            error!("Error receiving message? {e}");
-                            break;
-                        },
-                    };
-                },
-                _ = &mut timeout => {
-                    debug!("Max websocket time exceeded");
-                    crate::registry::TIMEOUTS.with_label_values(&["final"]).inc();
-                    break;
-                }
-                _ = &mut timeout_ping => {
-                    debug!("Max websocket ping time exceeded");
-                    crate::registry::TIMEOUTS.with_label_values(&["ping"]).inc();
-                    match websocket_send(&mut tx, Message::ping("livecount".as_bytes())).await {
-                        Err(e) => {
-                            warn!("Error sending ping on websocket: {e}");
-                            UPDATES_SENT.with_label_values(&["ping", &e]).inc();
-                            break;
-                        },
-                        Ok(_) => {
-                            UPDATES_SENT.with_label_values(&["ping", "ok"]).inc();
-                        },
-                    }
-                    new_sleep_ping = Some(MAX_WS_LIFE_PING);
-                }
-            };
-            if let Some(t) = new_sleep {
-                timeout.as_mut().reset(tokio::time::Instant::now() + t);
-            }
-            if let Some(t) = new_sleep_ping {
-                timeout_ping.as_mut().reset(tokio::time::Instant::now() + t);
+
+    // Sleep until connection times out. Gets reset on every incoming activity.
+    let new_sleep: Arc<tokio::sync::Mutex<Option<tokio::time::Instant>>> = Default::default();
+    let new_sleep_renew = || async {
+        (*new_sleep.lock().await) = Some(tokio::time::Instant::now() + MAX_WS_LIFE);
+    };
+    new_sleep_renew().await;
+
+    // Sleep until it's time to send a ping. Also gets reset on activity.
+    let new_sleep_ping: Arc<tokio::sync::Mutex<Option<tokio::time::Instant>>> = Default::default();
+    let new_sleep_ping_renew = || async {
+        (*new_sleep_ping.lock().await) = Some(tokio::time::Instant::now() + MAX_WS_LIFE_PING);
+    };
+    new_sleep_ping_renew().await;
+
+    // TODO: why 10?
+    let (to_client_tx, mut to_client_rx) = tokio::sync::mpsc::channel(10);
+
+    // Async that gets updates from the registry.
+    let from_registry = async {
+        while let Some(msg) = handle.next().await {
+            if let Err(e) = to_client_tx.send(Message::text(format!("{msg}"))).await {
+                error!("Failed to send to client-sender: {e:?}");
+                return Err("failed to send to client-sender".to_owned());
             }
         }
+        debug!("Registry closing");
+        Ok(())
+    };
+
+    // Async that actually sends on websocket.
+    let to_client = async {
+        while let Some(msg) = to_client_rx.recv().await {
+            match websocket_send(&mut tx, msg).await {
+                Err(e) => {
+                    warn!("Error sending on websocket: {e}");
+                    UPDATES_SENT.with_label_values(&["data", &e]).inc();
+                    return Err("sending on websocket".to_owned());
+                }
+                Ok(_) => {
+                    UPDATES_SENT.with_label_values(&["data", "ok"]).inc();
+                }
+            }
+        }
+        debug!("Nothing left to send to client");
+        Ok(())
+    };
+
+    // Async that reads from client.
+    let from_client = async {
+        loop {
+            let wsmsg = rx.next().await;
+            match wsmsg {
+                None => {
+                    debug!("Got None message, disconnecting");
+                    WS_RX_TYPE.with_label_values(&["none"]).inc();
+                    return Err::<(), _>("got None message".to_owned());
+                }
+                Some(Ok(ref m)) => {
+                    debug!("Got a message: {m:?}");
+                    new_sleep_ping_renew().await;
+                    if m.is_close() {
+                        debug!("WS Disconnection: {:?}", wsmsg);
+                        WS_RX_TYPE.with_label_values(&["away"]).inc();
+                        return Err("WS disconnection".to_owned());
+                    } else if m.is_ping() {
+                        WS_RX_TYPE.with_label_values(&["ping"]).inc();
+                        new_sleep_renew().await;
+                    } else if m.is_text() {
+                        WS_RX_TYPE.with_label_values(&["text"]).inc();
+                        new_sleep_renew().await;
+                    } else if m.is_binary() {
+                        WS_RX_TYPE.with_label_values(&["binary"]).inc();
+                        new_sleep_renew().await;
+                    } else if m.is_pong() {
+                        WS_RX_TYPE.with_label_values(&["pong"]).inc();
+                        new_sleep_renew().await;
+                    } else {
+                        WS_RX_TYPE.with_label_values(&["unknown"]).inc();
+                        error!("Unknown message type: {wsmsg:?}");
+                    }
+                }
+                Some(Err(e)) => {
+                    WS_RX_TYPE.with_label_values(&[format!("{e}")]).inc();
+                    error!("Error receiving message? {e}");
+                    return Err("error receiving message".to_owned());
+                }
+            };
+        }
+    };
+
+    // Async that triggers a total timeout since last activity.
+    // Any async returning Err will terminate all of them.
+    let f_timeout = async {
+        // Sleep as long as deadline keeps being updated.
+        loop {
+            let deadline = {
+                let mut s = new_sleep.lock().await;
+                match s.take() {
+                    None => break,
+                    Some(news) => news,
+                }
+            };
+            tokio::time::sleep_until(deadline).await;
+        }
+        debug!("Max websocket time exceeded");
+        TIMEOUTS.with_label_values(&["final"]).inc();
+        Err::<(), _>("timeout".to_owned())
+    };
+
+    // Async that triggers sending a ping.
+    let f_timeout_ping = async {
+        loop {
+            // Sleep as long as the deadline keeps getting updated.
+            loop {
+                let deadline = {
+                    let mut s = new_sleep_ping.lock().await;
+                    match s.take() {
+                        None => break,
+                        Some(news) => news,
+                    }
+                };
+                tokio::time::sleep_until(deadline).await;
+            }
+
+            // Send a ping.
+            debug!("Max websocket ping time exceeded");
+            TIMEOUTS.with_label_values(&["ping"]).inc();
+            match to_client_tx
+                .send(Message::ping("livecount".as_bytes()))
+                .await
+            {
+                Err(e) => {
+                    warn!("Error sending ping on websocket: {e}");
+                    UPDATES_SENT
+                        .with_label_values(&["ping", &format!("{e:?}")])
+                        .inc();
+                    return Err::<(), _>("error sending on websocket".to_owned());
+                }
+                Ok(_) => {
+                    UPDATES_SENT.with_label_values(&["ping", "ok"]).inc();
+                }
+            }
+
+            // We may not need another ping; if the first one is not replied to, why would a second
+            // one? But we don't want to busyloop nor exit the async.
+            new_sleep_ping_renew().await;
+        }
+    };
+
+    // Run all asyncs. If any of them return error, terminate them all.
+    if let Err(e) = tokio::try_join!(
+        from_registry,
+        to_client,
+        from_client,
+        f_timeout,
+        f_timeout_ping
+    ) {
+        debug!("WS asyncs ended with: {e:?}");
     }
+
     debug!("WS Terminating");
     handle.close().await;
 }
